7c6c85802be4a52c68cd908ff790fad588ec0bae
Antonina Yurchenko <tonya2512@yandex.ru>
2023-09-02 22:51:40 +0300

    .gitignore

--- src/ServiceProgram.java
+++ src/ServiceProgram.java
@@ -1,9 +1,10 @@
-public class Main {
+public class ServiceProgram {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         int weight = 98;
         double height = 1.87;
         int bmi = service.calculate(int weightInKg, double heightInMeters);
         System.out.println(bmi);
+
     }
 }
